0985ff05a6e9affe2933e677868e63a450c2f43f
seyityahya <seyityahya05@gmail.com>
2023-03-13 16:41:07 +0300

    19.project start

--- 19.project/src/App.js
+++ 19.project/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { GrHome } from "react-icons/gr";
 
 function App() {
+  const handleClick = () => {
+    console.log("deneme");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <link rel="stylesheet" />
+      <aside className="sidebar">
+        <header>
+          <GrHome />
+          <span>DataHub</span>
+        </header>
+        <button>
+          <span>home</span>
+          <span>Home</span>
+        </button>
+        <button onClick={handleClick()}>
+          <span>build</span>
+          <span>Tools</span>
+          <span>expand_more</span>
+        </button>
+        <div id="tools" className="subnav">
+          <div className="subnav-inner">
+            <button>
+              <span>Documents</span>
+            </button>
+          </div>
+        </div>
+      </aside>
     </div>
   );
 }
