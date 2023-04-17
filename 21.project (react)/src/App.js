d72975f88a2e4b52a3b6f66a99d8379e8b64a35e
seyityahya <seyityahya05@gmail.com>
2023-04-17 17:30:15 +0300

    21.project end

--- 21.project (react)/src/App.js
+++ 21.project (react)/src/App.js
@@ -23,12 +23,41 @@ function App() {
           name: "",
           lat: "",
           lon: "",
+          errors: {},
         },
       ],
     }));
   };
 
-  const enabled = route.route;
+  const handleStation = (value, name, key) => {
+    setRoute((route) => ({
+      ...route,
+      stations: route.stations.map((station, i) => {
+        if (key == i) {
+          station[name] = value;
+          let current = route.stations.find(
+            (r, index) => r[name] == value && key != index
+          );
+          if (current) {
+            station.errors[
+              name
+            ] = `${name} değeri başka bir alanda ${value} değeriyle zaten tanımlanmıştır!`;
+          } else {
+            delete station.errors[name];
+          }
+        }
+        return station;
+      }),
+    }));
+  };
+
+  const enabled =
+    route.route &&
+    route.stations.every((station) =>
+      Object.entries(station).every(([key, value]) =>
+        key == "errors" ? Object.values(value).length == 0 : value
+      )
+    );
 
   return (
     <>
@@ -40,11 +69,23 @@ function App() {
       />
       <button onClick={addStation}>Yeni Durak Ekle</button>
       <hr />
-      {route.stations.map((station) => (
+      {route.stations.map((station, key) => (
         <div>
-          <input type="text" placeholder="Durak Adı" />
-          <input type="text" placeholder="Enlem" />
-          <input type="text" placeholder="Boylam" />
+          <input
+            type="text"
+            onChange={(e) => handleStation(e.target.value, "name", key)}
+            placeholder="Durak Adı"
+          />
+          <input
+            type="text"
+            onChange={(e) => handleStation(e.target.value, "lat", key)}
+            placeholder="Enlem"
+          />
+          <input
+            type="text"
+            onChange={(e) => handleStation(e.target.value, "lon", key)}
+            placeholder="Boylam"
+          />
         </div>
       ))}
       <hr />
