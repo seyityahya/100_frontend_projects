574a9000debce8329cdec05f8d116b7b708f3015
seyityahya <seyityahya05@gmail.com>
2023-04-17 16:06:05 +0300

    21.project continue

--- 21.project (react)/src/App.js
+++ 21.project (react)/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [route, setRoute] = useState({
+    route: "",
+    stations: [],
+  });
+
+  const handleRoute = (e) => {
+    setRoute((route) => ({
+      ...route,
+      route: e.target.value,
+    }));
+  };
+
+  const addStation = () => {
+    setRoute((route) => ({
+      ...route,
+      stations: [
+        ...route.stations,
+        {
+          name: "",
+          lat: "",
+          lon: "",
+        },
+      ],
+    }));
+  };
+
+  const enabled = route.route;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="text"
+        value={route.route}
+        onChange={handleRoute}
+        placeholder="Güzergah"
+      />
+      <button onClick={addStation}>Yeni Durak Ekle</button>
+      <hr />
+      {route.stations.map((station) => (
+        <div>
+          <input type="text" placeholder="Durak Adı" />
+          <input type="text" placeholder="Enlem" />
+          <input type="text" placeholder="Boylam" />
+        </div>
+      ))}
+      <hr />
+      <button disabled={!enabled}>KAYDET</button>
+      <br />
+      <pre>{JSON.stringify(route, null, 2)}</pre>
+    </>
   );
 }
 
